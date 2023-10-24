ba7a6daafff35c5586cbedfa10295528b6b6eaba
Antonio Carlos Soares Prudencio <antonio.prudencio@maida.health>
2023-10-24 18:48:50 -0300

    first commit

---
+++ PrimeiroProjeto_dotNet/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int contador = 0;
+
+for(var i= 0; i < 10; i++)
+{
+    Console.WriteLine("Número: " + i);
+}
