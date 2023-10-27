e6b89f58bfccb63fe37ba492936adb1b9ee79d65
Antonio Carlos Soares Prudencio <acsprudencio@gmail.com>
2023-10-26 22:51:35 -0300

    aula 03

---
+++ aula03/Forma.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula03
+{
+    public class Forma
+    {
+        public string RetornaNome()
+        {
+            return "Forma de circular";
+        }
+        private int RetoraTamanho()
+        {
+            return 10;
+        }
+
+        public string Tipo()
+        {
+            return "Forma de pizza";
+        }
+        public string Tipo(string nome)
+        {
+            return "Forma de pizza";
+        }
+    }
+}
---
+++ aula03/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int numero1 = 10;
+int numero2 = numero1;
+
+Console.WriteLine("numero1: " + numero1);
+Console.WriteLine("numero2: " + numero2);
+
+numero2 = 20;
+
+Console.WriteLine("Depois da nova atribuição de numero2");
+Console.WriteLine("numero1: " + numero1);
+Console.WriteLine("numero2: " + numero2);
+
+
+numero1 = 15;
+
+Console.WriteLine("Depois da nova atribuição de numero1");
+Console.WriteLine("numero1: " + numero1);
+Console.WriteLine("numero2: " + numero2);
+
+string nome = "antonio";
+
+
+Console.WriteLine("Nome inicial: " + nome);
+
+mudarNome();
+
+Console.WriteLine("Nome atualizado: " + nome);
+
+void mudarNome(){
+    nome = "carlos";
+}
+
+
+
