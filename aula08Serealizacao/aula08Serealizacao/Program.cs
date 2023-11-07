0ef5884b7d3561a20adf08fcdda15dde9d857b83
Antonio Carlos Soares Prudencio <acsprudencio@gmail.com>
2023-11-06 21:08:55 -0300

    aula08 exercicio de serealizacao

---
+++ aula08Serealizacao/aula08Serealizacao/Program.cs
@@ -0,0 +1,33 @@
+﻿
+var pessoa = new Pessoa("Carlos",25);
+// var carro = new Carro("Corsa",2015);
+
+
+
+string jsonPessoa = System.Text.Json.JsonSerializer.Serialize(pessoa);
+Console.WriteLine(jsonPessoa);
+
+// string jsonPessoaInput = @"{""nome"":""Maria"",""Idade"":30}";
+// Pessoa pessoaDesserializada = System.Text.Json.JsonSerializer.Deserialize<Pessoa>(jsonPessoa);
+// Console.WriteLine($"{pessoaDesserializada.nome}, {pessoaDesserializada.idade}");
+
+var aquivo = new FileInfo("arquivo.txt");
+using (var writer = aquivo.CreateText())
+{
+    writer.Write(jsonPessoa);
+}
+
+
+
+public class Pessoa
+{
+    public Pessoa(string nome, int idade)
+    {
+        this.nome = nome;
+        this.idade = idade;
+    }
+    public string nome { get; set; }
+    public int idade { get; set; }
+}
+
+// public record Carro(string nome, int ano);
